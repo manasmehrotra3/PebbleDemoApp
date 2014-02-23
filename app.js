03233c270eb82fd2ea7fae6d162634d1cf4c243e
Manas Mehrotra <manasmehrotra3@gmail.com>
2014-02-23 23:19:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+/* global simply */
+
+simply.text({
+    title: 'High Performance.',
+    subtitle: 'Delivered.',
+}, true);
+
+simply.on('singleClick', function() {
+   simply.body("Okay! Good Job!"); 
+});
